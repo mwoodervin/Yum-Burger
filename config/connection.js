aec6f98c230aa0cac6b2a84ddc4917f58b57b802
Mary Wood <mwoodervin@gmail.com>
2020-09-19 22:41:51 -0400

    fixed connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,14 @@
 const mysql = require("mysql");
 
 // create connection with the database
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root12345",
-    database: "Yum_Burgers"
-});
+let connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
         host: 'localhost',
+        port: 3306,
         user: 'root',
         password: 'root12345',
         database: 'Yum_Burgers'
@@ -21,8 +16,12 @@ if (process.env.JAWSDB_URL) {
 }
 
 connection.connect(function (err) {
-    if (err) throw err;
+    if (err) {
+        console.err("error connecting " + err.stack);
+        return;
+    }
     console.log("connected as id " + connection.threadId);
+
 
 });
 
